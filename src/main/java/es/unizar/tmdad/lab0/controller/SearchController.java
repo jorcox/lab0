424d16cccbea7f69a6cb20e24bc07a9b3fe388e3
Jorge <646122@unizar.es>
2017-03-17 19:12:40 +0100

    RESTified app

--- src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
+++ src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
@@ -1,10 +1,13 @@
 package es.unizar.tmdad.lab0.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.unizar.tmdad.lab0.service.TwitterLookupService;
 
@@ -20,9 +23,10 @@ public class SearchController {
         return "index";
     }
 
+    @ResponseBody
     @RequestMapping("/search")
-    public String search(@RequestParam("q") String q, Model m) {
+    public SearchResults search(@RequestParam("q") String q, Model m) {
     	m.addAttribute("res", twitter.search(q));
-        return "search :: content";
+        return twitter.search(q);
     }
 }
--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -5,8 +5,26 @@ $(document).ready(function() {
 function registerSearch() {
 	$("#search").submit(function(ev){
 		event.preventDefault();
-		$.get($(this).attr('action'), {q: $("#q").val()}, function(data) {
-			$("#resultsBlock").empty().append(data);
+		$.getJSON($(this).attr("action"), {q: $("#q").val()}, function(data) {
+
+			var template = '{{#tweets}}'
+			+'<div class="row panel panel-default">'
+			+	    '<div class="panel-heading">'
+			+	        '<a href="https://twitter.com/{{fromUser}}"'
+			+	           'target="_blank"><b>@{{fromUser}}</b></a>'
+			+	        '<div class="pull-right">'
+			+	            '<a href="https://twitter.com/{{fromUser}}/status/{{idStr}}'
+			+	               'target="_blank"><span class="glyphicon glyphicon-link"></span></a>'
+			+	        '</div>'
+			+	    '</div>'
+			+	    '<div class="panel-body">{{unmodifiedText}}</div>'
+			+'</div>'
+			+'{{/tweets}}'
+
+			var html = Mustache.to_html(template, data);
+
+			$("#resultsBlock").empty().append(html);
+			
 		});	
 	});
 }
