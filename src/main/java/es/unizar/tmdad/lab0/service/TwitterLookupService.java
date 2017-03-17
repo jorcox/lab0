cc43429f2d74cc2a12f7bbc5eb4422d883333003
Jorge <646122@unizar.es>
2017-03-17 22:30:50 +0100

    Fixing Heroku

--- src/main/java/es/unizar/tmdad/lab0/service/TwitterLookupService.java
+++ src/main/java/es/unizar/tmdad/lab0/service/TwitterLookupService.java
@@ -5,18 +5,23 @@ import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class TwitterLookupService {
+	@Autowired
 	@Value("${twitter.consumerKey}")
 	private String consumerKey;
-	
+
+	@Autowired
 	@Value("${twitter.consumerSecret}")
 	private String consumerSecret;
-	
+
+	@Autowired
 	@Value("${twitter.accessToken}")
 	private String accessToken;
 	
+	@Autowired
 	@Value("${twitter.accessTokenSecret}")
 	private String accessTokenSecret;
 	
